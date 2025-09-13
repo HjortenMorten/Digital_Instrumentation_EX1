2ca9dca2c2a5e1eb5f97a12f99348890da11b164
HjortenMorten <119762706+HjortenMorten@users.noreply.github.com>
2025-09-13 12:24:46 +0200

    Delete Main_Files directory
    Upsi

--- Main_Files/Exercise0.2/main.c
+++
@@ -1,8 +0,0 @@
-#include "stm32f30x_conf.h" // STM32 config
-#include "30010_io.h" // Input/output library for this course
-int main(void)
-{
-uart_init( 9600 ); // Initialize USB serial emulation at 9600 baud
-printf("Hello World!\n"); // Show the world you are alive!
-while(1){}
-}
