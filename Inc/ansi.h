e9e1e1f5d40f7371c48b3a37a2a91551db2e0c0d
HjortenMorten <119762706+HjortenMorten@users.noreply.github.com>
2025-09-16 12:56:45 +0200

    Delete Inc directory

--- Inc/ansi.h
+++
@@ -1,4 +0,0 @@
-void initTimer();
-void initInterupt();
-void EXTI9_5_IRQHandler(void);
-void TIM2_IRQHandler(void);
--- Inc/gpio.h
+++
@@ -1,4 +0,0 @@
-void initJoystick();
-void initLed();
-void setLed(uint8_t joystick);
-uint8_t readJoystick();
